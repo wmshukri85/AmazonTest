4f8fa569df2b5e972050c52200aacb78ef9fee73
Shukri Rahman <shukri.wm@gmail.com>
2023-08-12 14:08:20 +0800

    First Commit

---
+++ Amazon01/TestCases/AmazonAssertPrice.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+
+namespace Amazon01.TestCases
+{
+    class AmazonAssertPrice
+    {
+        
+        IWebDriver driver;
+
+        [SetUp]
+        public void SetUpTest()
+        {
+            // Set up Chrome driver
+            driver = new ChromeDriver("C:\\Driver\\");
+            driver.Manage().Window.Maximize();           
+        }
+
+        [Test]
+        public void Test()
+        {          
+
+            // Navigate to Amazon
+            driver.Navigate().GoToUrl("https://www.amazon.com/");
+                        
+
+            // Find the search text box and type "laptop"
+            IWebElement searchBox = driver.FindElement(By.Id("twotabsearchtextbox"));
+            searchBox.SendKeys("laptop");
+
+            // Find the search button and click it
+            IWebElement searchButton = driver.FindElement(By.Id("nav-search-submit-button"));
+            searchButton.Click();
+
+            // Wait for search results to load
+            new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(driver => driver.Title.Equals("Amazon.com : laptop"));
+           
+            // Find the first search result and click it
+            IWebElement firstResult = driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[2]/div/span/a/div/img"));
+            firstResult.Click();                       
+
+            // Find the laptop price element
+            IWebElement priceElement = driver.FindElement(By.Id ("corePrice_feature_div"));
+
+            // Get the price text then replace newline with '.'            
+            string priceText = priceElement.Text.Replace(Environment.NewLine, ".");
+
+            // Convert the price string to a number
+            if (decimal.TryParse(priceText.Replace("$", ""), out decimal laptopPrice))
+            {
+                Console.WriteLine("laptopPrice: " + laptopPrice);
+
+                // Assert that the price is more than $100
+                Assert.Greater(laptopPrice, 100);
+            }
+            else
+            {
+                Console.WriteLine("laptopPrice: " + laptopPrice);
+                Assert.Fail("Unable to parse laptop price.");
+            }
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            // Close the browser
+            driver.Quit();        
+        }
+    }
+}
